7170999da5ffe09e1328c67521801125728fba75
Shahriar Ahmed Shovon <shovonshahriar01@gmail.com>
2022-11-25 21:53:41 +0600

    to add in repo

--- script.js
+++ script.js
@@ -2,15 +2,52 @@
 
 const textRef = document.getElementById("typing-ref")
 const textInput = document.getElementById("typing-input")
+const textInputShowcase = document.getElementById("typing-input-showcase")
 
+const refText = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam totam
+eos, alias adipisci atque asperiores explicabo corrupti, porro beatae
+facilis ad cum praesentium pariatur deleniti natus quos eum odit.
+Saepe.`
 
-const typingRefText = [...textRef.textContent]
+refText.split("").forEach(let => {
+    let letterSpan = document.createElement("span")
+    if (let == " ") {
+        letterSpan.innerHTML = "&nbsp;"
+    } else {
+        letterSpan.innerText = let
+    }
 
-// typingRefText.forEach(eachLetter => {
-//     console.log(eachLetter)
-// })
+    textRef.appendChild(letterSpan)
+})
 
 function onType() {
     const typed = textInput.value
-    console.log(typed[typed.length - 1])
+    const lastTypedLetter = typed[typed.length - 1]
+    const lastRefLetter = textRef.childNodes[typed.length - 1]
+
+    lastRefLetter.style.color = "white"
+
+    if (lastTypedLetter !== lastRefLetter.textContent) {
+        console.log("Wrong")
+    }
+
+    textInputShowcase.innerHTML = ""
+
+    typed.split("").forEach(let => {
+        let typedLetterSpan = document.createElement("span")
+        if (let == " ") {
+            typedLetterSpan.innerHTML = "&nbsp;"
+        } else {
+            typedLetterSpan.innerText = let
+        }
+    
+        textInputShowcase.appendChild(typedLetterSpan)
+    })
+
+    // console.log("From Ref: ", lastRefLetter.textContent)
+    // console.log("From Input: ", lastTypedLetter)
 }
+
+textInput.addEventListener("input", () => {
+    onType()
+})
